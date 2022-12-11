49f8dc19e0c90534da71cc7d7d25053c79acf002
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-12-11 11:01:07 +0000

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/unsubscribe'
+  end
+  # 顧客用
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  namespace :public do
+    get 'rooms/index'
+    get 'rooms/show'
+    get 'rooms/new'
+    get 'rooms/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
