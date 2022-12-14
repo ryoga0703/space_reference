724494558538c9621a3fee95a377accc3582aa61
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-12-14 13:21:30 +0000

    部屋新規登録画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     root :to =>"homes#top"
     get "about"=>"homes#about"
 
-    resources :rooms, only: [:index,:show,:new,:edit,:update,:destroy]
+    resources :rooms, only: [:index,:show,:new,:create,:edit,:update,:destroy]
 
     get 'customers/unsubscribe' => 'customers#unsubscribe'
     patch 'customers/withdraw' => 'customers#withdraw'
