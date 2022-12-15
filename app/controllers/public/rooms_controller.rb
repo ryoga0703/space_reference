d7d6fffe8d8e844b68e344529d8dcbe18230c458
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-12-15 15:46:47 +0000

    cocoon　レイアウト途中

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -33,7 +33,7 @@ class Public::RoomsController < ApplicationController
   def room_params
     # album_tracks_attributesが子のモデルに保存する要素
     #   :id, :_destroyをつけることで、編集と削除が可能になる
-    params.require(:room).permit(:introduction, :room_image, :taste_id, :customer_id,
+    params.permit(:introduction, :room_image,
     furnitures_attributes: [:id, :name, :price, :introduction, :retailer_link, :_destroy])
   end
 end
--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -1,3 +1,5 @@
 class Furniture < ApplicationRecord
+  has_one_attached :furniture_image
+
   belongs_to :room
 end
