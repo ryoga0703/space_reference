da69e016c233c4fa4c98766e885fe9df793ae19c
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-12-25 06:24:52 +0000

    taste_idとcustomer_idからdefault: ""

--- db/migrate/20221211143513_create_rooms.rb
+++ db/migrate/20221211143513_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :rooms do |t|
-      t.integer :taste_id,          null: false, default: ""
-      t.integer :customer_id,       null: false, default: ""
+      t.integer :taste_id,          null: false
+      t.integer :customer_id,       null: false
       t.text :introduction,         null: false, default: ""
       t.timestamps
     end
