f32d79fd3aeb7802e175a8c2163c4a4b998c40f3
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-12-25 06:35:45 +0000

    introductionのdefault: ""を削除

--- db/migrate/20221211143513_create_rooms.rb
+++ db/migrate/20221211143513_create_rooms.rb
@@ -3,7 +3,7 @@ class CreateRooms < ActiveRecord::Migration[6.1]
     create_table :rooms do |t|
       t.integer :taste_id,          null: false
       t.integer :customer_id,       null: false
-      t.text :introduction,         null: false, default: ""
+      t.text :introduction,         null: false
       t.timestamps
     end
   end
